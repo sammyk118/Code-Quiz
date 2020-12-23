3ce83d2144d89b84b435b67ca0a50dd4007906ef
Sammy Kroner <sammyk118@gmail.com>
2020-12-22 17:32:22 -0800

    added question structure

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 var begin = document.querySelector("#start");
-var timerEl = document.querySelector("#countdown");
+var timerEl = document.querySelector("#timer");
 var questionSect = document.querySelector("#questionSect");
 var choicesList = document.querySelector("#choicesList");
+
+
+//array of objects to hold all the question data
+//question, choices, answer
+var questions = [
+    {
+        question: "Commonly used data types do NOT include: ",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+    {
+        question: "The condition in an if/else statement is enclosed within ____",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "string values must be enclosed within ____ when being assigned to variables",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        question: "A very useful too used during web development and debugging for printing content to the debugger is ",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }];
+
 
 var score = 0;
 var currIndex = 0;
@@ -22,6 +54,9 @@ begin.addEventListener("click", function (event) {
 });
 
 
-function generate() {
+function generate(currIndex) {
     
 }
+
+
+
